1c2bf7d9ae1c499fe501a816903d82a923c23e4c
Jin Kwon <onacit@gmail.com>
2025-12-19 22:45:32 +0900

    wip

---
+++ src/test/java/io/github/jinahya/rickandmortyapi/persistence/EpisodeCharacterId_Test.java
@@ -0,0 +1,8 @@
+package io.github.jinahya.rickandmortyapi.persistence;
+
+class EpisodeCharacterId_Test extends __Base_Test<EpisodeCharacterId> {
+
+    EpisodeCharacterId_Test() {
+        super(EpisodeCharacterId.class);
+    }
+}
---
+++ src/test/java/io/github/jinahya/rickandmortyapi/persistence/LocationResidentId_Test.java
@@ -0,0 +1,8 @@
+package io.github.jinahya.rickandmortyapi.persistence;
+
+class LocationResidentId_Test extends __Base_Test<LocationResidentId> {
+
+    LocationResidentId_Test() {
+        super(LocationResidentId.class);
+    }
+}
