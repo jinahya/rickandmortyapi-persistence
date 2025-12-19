9165eec76470801f120499209e717fddee1d881e
Jin Kwon <onacit@gmail.com>
2025-12-19 14:29:54 +0900

    wip

--- src/main/java/io/github/jinahya/rickmortyapi/persistence/_BaseEntity.java
+++ src/main/java/io/github/jinahya/rickmortyapi/persistence/_BaseEntity.java
@@ -11,7 +11,7 @@ import jakarta.persistence.MappedSuperclass;
 @SuppressWarnings({
         "java:S101" // Class names should comply with a naming convention
 })
-abstract class _BaseEntity {
+public abstract class _BaseEntity {
 
     // -------------------------------------------------------------------------------------------------------- BUILDERS
 
@@ -22,7 +22,7 @@ abstract class _BaseEntity {
     /**
      * Creates a new instance.
      */
-    _BaseEntity() {
+    protected _BaseEntity() {
         super();
     }
 }
