38b2b211f68a08775e725f10cf55a523f76df507
Jin Kwon <onacit@gmail.com>
2026-01-13 10:04:38 +0900

    wip

---
+++ src/test/java/io/github/jinahya/rickandmortyapi/persistence/Character_FindAllByType_PersistenceTest.java
@@ -0,0 +1,122 @@
+package io.github.jinahya.rickandmortyapi.persistence;
+
+/*-
+ * #%L
+ * rickandmortyapi-persistence
+ * %%
+ * Copyright (C) 2025 GitHub, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import io.github.jinahya.rickandmortyapi.persistence.mapped.Character_Type;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 재한님
+@Slf4j
+@SuppressWarnings({
+        "java:S117" // Local variable and method parameter names should comply with a naming convention
+})
+class Character_FindAllByType_PersistenceTest
+        extends _BaseEntity_Persistence_<Character, Integer> {
+
+    // ---------------------------------------------------------------------------------------------------- CONSTRUCTORS
+    Character_FindAllByType_PersistenceTest() {
+        super(Character.class, Integer.class);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @EnumSource(Character_Type.class)
+    @ParameterizedTest
+    void _QueryLanguage_(final Character_Type type) {
+        // --------------------------------------------------------------------------------------------------- given
+        final var firstResult = ThreadLocalRandom.current().nextBoolean()
+                ? null
+                : ThreadLocalRandom.current().nextInt(1, 10);
+        final var maxResults =
+                ThreadLocalRandom.current().nextBoolean()
+                        ? null
+                        : ThreadLocalRandom.current().nextInt(1, 10);
+        // ---------------------------------------------------------------------------------------------------- when
+        final var result = applyEntityManager(em -> {
+            final var query = em.createQuery(
+                    """
+                            SELECT c
+                            FROM Character c
+                            WHERE c.type = :type
+                            ORDER BY c.id ASC""",
+                    Character.class
+            );
+            query.setParameter("type", type);
+            Optional.ofNullable(firstResult).ifPresent(query::setFirstResult);
+            Optional.ofNullable(maxResults).ifPresent(query::setMaxResults);
+            return query.getResultList();
+        });
+        // ---------------------------------------------------------------------------------------------------- then
+        if (firstResult != null && result.isEmpty()) {
+            return;
+        }
+        assertThat(result)
+                .isSortedAccordingTo(Character.COMPARING_ID)
+                .extracting(Character::getType)
+                .containsOnly(type);
+        if (maxResults != null) {
+            assertThat(result).hasSizeLessThanOrEqualTo(maxResults);
+        }
+    }
+
+    @EnumSource(Character_Type.class)
+    @ParameterizedTest
+    void _CriteriaApi_(final Character_Type type) {
+        // --------------------------------------------------------------------------------------------------- given
+        final var firstResult = ThreadLocalRandom.current().nextBoolean()
+                ? null
+                : ThreadLocalRandom.current().nextInt(1, 10);
+        final var maxResults =
+                ThreadLocalRandom.current().nextBoolean()
+                        ? null
+                        : ThreadLocalRandom.current().nextInt(1, 10);
+        // ---------------------------------------------------------------------------------------------------- when
+        final var result = applyEntityManager(em -> {
+            final var builder = em.getCriteriaBuilder();
+            final var query = builder.createQuery(Character.class);
+            final var root = query.from(Character.class);
+            query.where(builder.equal(root.get(Character_.type), type));
+            query.orderBy(builder.asc(root.get(Character_.id)));
+            final var typed = em.createQuery(query);
+            Optional.ofNullable(firstResult).ifPresent(typed::setFirstResult);
+            Optional.ofNullable(maxResults).ifPresent(typed::setMaxResults);
+            return typed.getResultList();
+        });
+        // ---------------------------------------------------------------------------------------------------- then
+        if (firstResult != null && result.isEmpty()) {
+            return;
+        }
+        assertThat(result)
+                .isSortedAccordingTo(Character.COMPARING_ID)
+                .extracting(Character::getType)
+                .containsOnly(type);
+        if (maxResults != null) {
+            assertThat(result).hasSizeLessThanOrEqualTo(maxResults);
+        }
+    }
+}
